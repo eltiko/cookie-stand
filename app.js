db402a32cb5ec756b9634501f9d9c67f35b722ed
eltiko <elticco@hotmail.com>
2019-10-16 22:29:50 -0700

    updates

--- app.js
+++ app.js
@@ -1,111 +1,105 @@
 'use strict';
 
 var cookieShopHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-var cookieShopAllStore = [];
-var cookieShopTableData = document.getElementById('table');
-var cookieShopAllStoreTotal = 0;
+var CookieTable = document.getElementById('table');
+var renderHeadRow = function(){
 
-//code review constructor function and then create the header, renderShopRow, renderFooterRow
-function CookieShop(minCust, maxCust, avgCookie, locName) {
-    this.minCust = minCust;
-    this.maxCust = maxCust;
-    this.avgCookie = avgCookie;
-    this.locName = locName;
-    this.hourlyCookieTotal = [];
-    this.dailyCookie = 0;
-    cookieShopAllStore.push(this);{
+var trEl = document.createElement('tr');
+var thEl = document.createElement('th');
+thEl.textContent = 'Locations';
+trEl.appendChild(thEl);
 
+for (var i = 0; i < cookieShopHours.length; i++){
+    var tdElement = document.createElement('td');
+    tdElement.textContent = cookieShopHours[i];
+    trEl.appendChild(tdElement);
     }
-    //generate cookies upon instantiation
-    this.generateHourlyCookie();
-    //generate shop row upon instatiation
-    this.renderShopRow();
+     tdElement = document.createElement('td');
+    tdElement.textContent = 'Daily Total';
+    trEl.appendChild(tdElement);
+    CookieTable.appendChild(trEl)
 }
+renderHeadRow();
 
-var randomCustomer = function(min, max) {
-    return Math.random * ((max-min) + 1) + min;
-};
-
-CookieShop.prototype.generateHourlyCookie = function() {
-    for (var i = 0; i < cookieShopHours.length; i++){
-        var cookie = Math.ceil(this.avgCookie * randomCustomer(this.minCust, this.maxCust));
-        this.hourlyCookieTotal.push(cookie);
-        this.dailyCookie += cookie;
-        cookieShopAllStore += cookie;
-    }
-};
-
-var renderHeaderRow = function() {
-    var trEl = document.createElement('tr')
-    var thEl = document.createElement('th')
-    thEl.textContent = 'Location';
-    trEl.appendChild(thEl);
-
-    for( var i = 0; i < cookieShopHours.length; i++) {
-        var tdEl = document.createElement('td');
-        tdEl.textContent = cookieShopHours[i];
-        trEl.appendChild(tdEl);
-    }
-
-    var tdEl1 = document.createElement ('td')
-    tdEl1.textContent = 'Total';
-    trEl.appendChild(tdEl1);
-    cookieShopTableData.appendChild(trEl);
-};
-
-renderHeaderRow();
-
-CookieShop.prototype.renderShopRow = function(){
-//create elements
-var trEl = document.createElement ('tr')
-var tdEl = document.createElement('td')
-tdEl.textContent = this.locName;
-trEl.appendChild(tdEl);
-
-//loop over the length of hourlyCookiesTotal and give content 
-// Ron - we have to adjust our elements to ; addElement.element to access the newly made elements
-for( var i = 0; i < this.hourlyCookieTotal.length; i++) {
-    // crest the element
-    tdEl = document.createElement('td');
-    //give content to th element 
-    tdEl.textContent = this.hourlyCookieTotal[i];
-    trEl.appendChild(tdEl);
+var allStores = [];
+function Stores(location, minCust, maxCust, avgCookies){
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookies = avgCookies;
+  this.cookiesHours = [];
+  this.dailySale = 0;
+  
+  this.sumCookie = function(){
+      for( var i = 0; i < cookieShopHours.length; i++){
+          var randomNumber = Math.floor(Math.random()*(this.maxCust - this.minCust)+ this.minCust);
+          this.cookiesHours.push(randomNumber);
+      }
+  };
+  this.totalDay = function(){
+      for( var i = 0; i < cookieShopHours.length; i++){
+          this.dailySale += this.cookiesHours[i];
+      }
+  };
+  this.sumCookie();
+  this.totalDay();
+  allStores.push(this); 
 }
-//accessing this.dailyCookievalue to sow total
-var tdEl1 = document.createElement('td');
-tdEl1.textContent = this.dailyCookie;
-trEl.appendChild(tdEl1);
-cookieShopTableData.appendChild(trEl);
+//get the render
+Stores.prototype.render = function(){
+//creat a table row for data to store in
+var trEl = document.createElement('tr');
+var thEl = document.createElement('th');
+this.textContent = this.location;
+trEl.appendChild(thEl);
+
+for( var i = 0; i < cookieShopHours.length; i++){
+    var tdElement = document.createElement('td');
+    tdElement.textContent = this.cookiesHours[i];
+    trEl.appendChild(tdElement);
+}
+var sumElement1 = document.createElement('td');
+    sumElement1.textContent = this.dailySale;
+    trEl.appendChild(sumElement1);
+    CookieTable.appendChild(trEl);
 };
+var Seattle = new Stores('Seattle', 23, 65, 6.3);
+var Tokyo = new Stores('Tokyo', 3, 24, 1.2);
+var Dubai = new Stores('Dubai', 11, 38, 3.7);
+var Paris = new Stores('Paris', 20, 38, 3.2);
+var Lima = new Stores('Lima', 2, 16, 4.6);
 
-var dubai = new CookieShop(48, 155, 2.2, 'Dubai');
+Seattle.render();
+Tokyo.render();
+Dubai.render();
+Paris.render();
+Lima.render();
 
-
-
-//does not need to be prototype because we don't acces 'this'
-var renderFooterRow = function() {
+var totalRow = function () {
     var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td');
-    tdEl.textContent = 'Hourly Totals';
-    trEl.appendChild(tdEl);
-
-    for( var i = 0; i < cookieShopHours.length; i++); {
-        var storeHourlyTotals = 0;
-        var tdEl2 = document.createElement ('td');
-
-        for( var x = 0; x < cookieShopAllStore.length; x++) {
-            storeHourlyTotals += cookieShop.allStore[x].hourlyCookieTotal[i];
-        }
-        tdEl2.textContent = storeHourlyTotals;
-        trEl.appendChild(tdEl2);
+    var tdElement = document.createElement('td');
+    tdElement.textContent = 'Total:';
+    trEl.appendChild(tdElement);
+  
+    for (var i = 0; i < cookieShopHours.length; i++) {
+      var tdElement = document.createElement('td');
+      var newTotal = 0;
+      for (var x = 0; x < allStores.length; x++) {
+        newTotal += allStores[x].cookiesHours[i];
+        tdElement.textContent = newTotal;
+        trEl.appendChild(tdElement);
+      }
     }
-
-    var tdEl3 = document.createElement('td');
-    tdEl3.textContent = cookieShopAllStoreTotal;
-    trEl.appendChild(tdEl3);
-    cookieShopTableData.appendChild(trEl);
-
-};
-// new CookieShop(34, 13, 2.1, 'Seattle');
-console.log(dubai);
-
+  
+    var finalTotal = 0;
+    for (var k = 0; k < allStores.length; k++) {
+      finalTotal += allStores[k].dailySale;
+    }
+    var lastTdRow = document.createElement('td');
+    lastTdRow.textContent = finalTotal;
+    trEl.appendChild(lastTdRow);
+  
+    CookieTable.appendChild(trEl);
+  };
+  
+  totalRow();
